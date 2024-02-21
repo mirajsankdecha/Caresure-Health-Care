4732287b17074aa8193980f7b018dd0794036327
mirajsankdecha <mirajsankdecha2gmail.com>
2024-02-21 12:51:26 +0530

    Code Update

--- contact.php
+++ contact.php
@@ -1,2 +1,12 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "caresure");
+if (isset($_POST['btnSubmit'])) {
+	$name = $_POST['txtName'];
+	$email = $_POST['txtEmail'];
+	$contact = $_POST['txtPhone'];
+	$message = $_POST['txtMsg'];
+
+	$query = "insert into contact(name,email,contact,message) values('$name','$email','$contact','$message');";
+	$result = mysqli_query($con, $query);
 
 
